dac92ceee94d450aa22419645b7b73bd11b346b7
lazuardi_fenjano <lazuardifenzan@gmail.com>
2022-01-03 16:12:05 +0700

    Geolocation Test

---
+++ LocationSampleApp/LocationSampleApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator;
+using Xamarin.Essentials;
+
+namespace LocationSampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isGettingLocation;
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        async void BtnGetLocation_Clicked(object sender, EventArgs e)
+        {
+            resultLocation.Text = "";
+            isGettingLocation = true;
+            
+            while(isGettingLocation)
+            {
+                var result = await Geolocation.GetLocationAsync(new
+                GeolocationRequest(GeolocationAccuracy.Default, TimeSpan.FromSeconds(30)));
+
+                resultLocation.Text += $"lat: {result.Latitude}, lng: {result.Longitude} {Environment.NewLine}";
+                await Task.Delay(1000);
+            }
+            
+        }
+        void BtnStopLocation_Clicked(object sender, EventArgs e)
+        {
+            isGettingLocation=false;
+        }
+
+    }
+}
